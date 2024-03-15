f8ff67a52a34994b3b3e5fcae21e70ce555833eb
Mixowo <mixowo@hotmail.com>
2024-03-15 13:54:21 +0100

    Updated the name of the MockToken used for the payback beta testing period

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -6,9 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @notice Contract that deploys a ERC20 token for testing the Staking Rewards contract.
 
 contract MockToken is ERC20 {
-    constructor() ERC20("MockToken", "MTK") {}
+  constructor() ERC20("VaiTesting", "VAIT") {}
 
-    function mint(address account, uint256 amount) public {
-        _mint(account, amount);
-    }
+  function mint(address account, uint256 amount) public {
+    _mint(account, amount);
+  }
 }
